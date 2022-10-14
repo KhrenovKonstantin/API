7431f17969e00c4b8a76cccad7afe6ba9c01aa95
KhrenovKonstantin <etayazzaza@yandex.ru>
2022-10-14 16:39:24 +0500

    2

--- test_update_os.py
+++ test_update_os.py
@@ -52,7 +52,7 @@ class Test_update_osk(BaseCase):
                     "AWKEY": "09072021/2",
                     "GJAHR": "2021",
                     "ACT-ID": "pre09072021/2",
-                    "SERNR": "123йцу123йу1",
+                    "SERNR": "666",
                     "row": 1,
                     "deleted": ""
                 }
@@ -67,6 +67,7 @@ class Test_update_osk(BaseCase):
         for result in obj['result']:
             assert result['MESSAGE'] != "", f"The value of 'MESSAGE' is not correct"
             assert result['TYPE'] == "S", result['MESSAGE']
+            print(result['MESSAGE'])
         #assert obj['MESSAGE'] != None, f"The value of 'message' is not correct"
         #assert obj['error'] is None, obj['error']
 
@@ -360,3 +361,50 @@ class Test_update_osk(BaseCase):
         assert obj['status'] == 200, obj['error']
         #assert obj['MESSAGE'] != None, f"The value of 'message' is not correct"
         #assert obj['error'] is None, obj['error']
+
+    @allure.description('Актуализация атрибутов и статусов ОС, item.OS_STATUS == Перемещен и item.DEAKT не пусто')
+    def test_eith_responce(self):
+        json_data = {
+            "currentPage": 1,
+            "pageCount": 1,
+            "delta": "F",
+            "item": [
+                {
+                    "ANLN1": "240000240179",
+                    "TXT50": "Аппарат телефонный Cisco/CP-7945G=13",
+                    "INVNR": "12121313",
+                    "AKTIV": "29.03.2016",
+                    "KOSTL": "1006103050",
+                    "WERKS": "1002",
+                    "STORT": "43",
+                    "ANSW_END": "20000.00",
+                    "RBW_END": "10000",
+                    "MENGE": "2",
+                    "POSNR": "1100648.80016-YO04-00013",
+                    "GLO_RUS_OKOF16": "330.28.12",
+                    "GLO_RUS_DEPGRN": "03",
+                    "NDJAR": "003",
+                    "DEAKT": "13.02.2023",
+                    "KTEXT3": "г.Санкт-Петербург ул.Почтамтская 3-5",
+                    "ZZCNDPERNR": "00028076",
+                    "CNDPERNR_NAME": "Дернов ИН123",
+                    "OS_STATUS": "Перемещен",
+                    "AWKEY": "09072021/2",
+                    "GJAHR": "2021",
+                    "ACT-ID": "pre09072021/2",
+                    "SERNR": "666",
+                    "row": 1,
+                    "deleted": ""
+                }
+            ]
+        }
+        response = MyRequests.post(self.url, json=json_data, headers=self.header, cookies=self.jar)
+        Assertion.assert_code_status(response, 200)
+
+        print(response.text)
+
+        obj = json.loads(response.text)
+        for result in obj['result']:
+            assert result['MESSAGE'] != "", f"The value of 'MESSAGE' is not correct"
+            assert result['TYPE'] == "S", result['MESSAGE']
+            print(result['MESSAGE'])
